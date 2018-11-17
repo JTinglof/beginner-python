c682fcb417965e9d48ce64554f779bcf09b35adf
Joseph Tinglof <jbtinglof@gmail.com>
2018-11-16 17:48:29 -0800

    Update prog4_1.py

--- stack-machine/prog4_1.py
+++ stack-machine/prog4_1.py
@@ -2,8 +2,6 @@ import sys
 
 file_name = sys.argv[1]             #stores the file name from the command line
 
-print("Assignment #4-1, Joseph Tinglof, jbtinglof@gmail.com")
-
 [print(','.join(x.split())) for x in open(file_name).readlines() if len(x.split())>0] 
                                     #Looks for whitespace to tokenize at and inserts the ","
 
